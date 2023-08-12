3a968e53efc671b4c7ec0736829301015b4b4f07
ayachinene0100 <ayachinene0100@163.com>
2023-08-12 16:00:32 +0800

    修复Controller返回类型为String时转换异常的问题

--- src/main/java/org/ruisnow/ruisnow/controller/support/ResultAdvice.java
+++ src/main/java/org/ruisnow/ruisnow/controller/support/ResultAdvice.java
@@ -1,23 +1,39 @@
 package org.ruisnow.ruisnow.controller.support;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.core.MethodParameter;
 import org.springframework.http.MediaType;
 import org.springframework.http.converter.HttpMessageConverter;
 import org.springframework.http.server.ServerHttpRequest;
 import org.springframework.http.server.ServerHttpResponse;
+import org.springframework.lang.NonNull;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseBodyAdvice;
 
 @RestControllerAdvice
 public class ResultAdvice implements ResponseBodyAdvice<Object> {
 
+    private static final Logger logger = LoggerFactory.getLogger(ResultAdvice.class);
+
     @Override
-    public boolean supports(MethodParameter returnType, Class<? extends HttpMessageConverter<?>> converterType) {
+    public boolean supports(@NonNull MethodParameter returnType, @NonNull Class<? extends HttpMessageConverter<?>> converterType) {
         return true;
     }
 
     @Override
-    public Object beforeBodyWrite(Object body, MethodParameter returnType, MediaType selectedContentType, Class<? extends HttpMessageConverter<?>> selectedConverterType, ServerHttpRequest request, ServerHttpResponse response) {
+    public Object beforeBodyWrite(Object body, @NonNull MethodParameter returnType, @NonNull MediaType selectedContentType, @NonNull Class<? extends HttpMessageConverter<?>> selectedConverterType, @NonNull ServerHttpRequest request, @NonNull ServerHttpResponse response) {
+        if (body instanceof String) {
+            ObjectMapper mapper = new ObjectMapper();
+            try {
+                return mapper.writeValueAsString(Result.ok(body));
+            } catch (JsonProcessingException e) {
+                logger.error("Failed to convert response body {} to Json string.", body, e);
+            }
+        }
+
         return Result.ok(body);
     }
 }
