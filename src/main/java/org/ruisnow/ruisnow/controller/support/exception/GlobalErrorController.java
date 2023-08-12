aa2dec127d6e76957d74c3db21553544a44f4529
ayachinene0100 <ayachinene0100@163.com>
2023-08-12 15:21:37 +0800

    打印异常堆栈

--- src/main/java/org/ruisnow/ruisnow/controller/support/exception/GlobalErrorController.java
+++ src/main/java/org/ruisnow/ruisnow/controller/support/exception/GlobalErrorController.java
@@ -1,13 +1,38 @@
 package org.ruisnow.ruisnow.controller.support.exception;
 
+import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Before;
 import org.ruisnow.ruisnow.controller.support.Code;
 import org.ruisnow.ruisnow.controller.support.ErrorResult;
 import org.ruisnow.ruisnow.controller.support.Result;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Component;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 @RestControllerAdvice
 public class GlobalErrorController {
+
+    private static final Logger logger = LoggerFactory.getLogger(GlobalErrorController.class);
+
+    @Aspect
+    @Component
+    private static class HandleErrorAspect {
+
+        @Before("execution(" +
+                "public org.ruisnow.ruisnow.controller.support.ErrorResult " +
+                "org.ruisnow.ruisnow.controller.support.exception.GlobalErrorController." +
+                "handleError(..))")
+        public void beforeHandle(JoinPoint joinPoint) {
+            for (Object arg : joinPoint.getArgs()) {
+                if (arg instanceof Throwable) {
+                    logger.error("Error occurred!", (Throwable) arg);
+                }
+            }
+        }
+    }
 
     @ExceptionHandler(Throwable.class)
     public ErrorResult handleError(Throwable th) {
